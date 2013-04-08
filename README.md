675b338585f362ffd27148a311c03ba98f6dff6f
Christian Wolff <chris@connye.com>
2013-04-08 10:34:27 +0200

    Inital Commit of t3dump

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-t3dump
-======
+Typo3 Dump Script
+=================
 
-Typo3 SQL Dump Script
+This script offers an easy way to create an database dump of an Typo3 Installation.
+the script uses your install tool password to protects unauthorized persons from creating dumps on your system.
+
+
+Requirments
+-----------
+
+  * PHP 5.0 or Higher 
+  * mySQLdump (commandline tool should be normaly installed)
+  * PHP needs to be able to call the exec() function.
+
+Installation & Usage
+--------------------
+
+  - Copy the script to typo3conf/ directory of your typo3 installation.
+  - call the script like http://example.com/typo3conf/t3dump.php
+  - enter your install tool password and klick "Create Dump"
+  - your database dump should be created.
+
+Configuration (Optional)
+------------------------
+
+if you experience any problems with the "installation & Usage section" it might be you need to configure some 
+details open the t3dump script. there should be a section labled "### Configuration Options" where you 
+could set some details like the path to your mysqldump script. or another path for storing the sql dumps.
+
---
+++ t3dump.php
@@ -0,0 +1,95 @@
+<?php 
+
+/**
+ * Typo3 DB Dump Script 
+ * This Script creates a SQL dump of your mysql Datebase
+ * it uses localconf.php to detect database setting and
+ * mysqldump to create the dump of the databse.
+ * 
+ * version: 1.2
+ */
+ 
+
+### Configuration Options
+$path_msqldump =''; /* 	this is only needet if mysqldump is not in PATH envirionment variable (c:\xampp\mysql\bin\mysqldump.exe) */
+$overwrite_password =''; /* If this is set this password is used instad of the install tool password (md5) */
+$path_localconf ='localconf.php'; // include, path for localconf
+$controllBytes = 200; // bytes to show from the end of the dump;
+$path_dump_store = dirname(__FILE__);
+
+
+
+### Starting Processing
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>T3 MySQL Dump Script</title>
+<style type="text/css">
+	.error {color:red;}
+	.control-output {
+		margin:10px;
+		padding:10px;
+		background:#eee;
+		border:1px solid #999;
+	}
+</style>
+</head>
+<body>
+<?php 
+
+
+if(is_readable($path_localconf) ){
+	require_once($path_localconf);
+	$checkpw = $TYPO3_CONF_VARS['BE']['installToolPassword'];
+	if($overwrite_password != ''){
+		$checkpw=md5($overwrite_password);
+	}
+	if(!empty($_REQUEST['pwd']) and md5($_REQUEST['pwd']) == $checkpw){ 
+		$dumpname = $path_dump_store .'/'. date('Y-m-d-His-').$typo_db .'.sql';
+
+		$dumpOpt = " -u $typo_db_username -p$typo_db_password -h$typo_db_host -r\"$dumpname\" $typo_db ";
+		if(file_exists($dumpname)){
+			die("dumpfile already exists: $dumpname");
+		}
+		if(empty($path_msqldump)){
+			$path_msqldump = 'mysqldump';
+		}
+		$ausgabe = array();
+		$returnVar = 0;
+		echo "<h1>Creating dump</h1>"; //<p>using: $path_msqldump $dumpOpt <br/> $dumpname</p>
+		$ret = exec($path_msqldump.$dumpOpt ,$ausgabe, $returnVar );
+			
+		if(file_exists($dumpname)){
+			echo "<p> file <i>$dumpname</i> created! <br />filesize: " .number_format(filesize($dumpname),0,',','.'). ' Bytes</p>';
+			echo 'controll output (last lines of dump):<pre class="control-output">';
+			$fp = fopen($dumpname,'r');
+			fseek($fp,-$controllBytes,SEEK_END);
+			$text = fread($fp,$controllBytes);
+			fclose($fp);
+			echo str_replace("Dump completed","<b>Dump completed</b>",htmlentities($text));
+			echo '</pre>';
+		}else{
+			echo '<p class="error"><strong>ERROR: no Dump File Created!!!</strong></p>';
+		}
+		
+	}else{
+		echo <<< ENDFORM
+			<form action="" method="post">
+				<fieldset>
+					<legend>Create Typo3 Database Dump (v1.0)</legend>
+					<p>the install tool password is required to create a dump</p>
+					<label>Install tool Password: <input type="password"  value="" name="pwd" /></label><br />
+					<!--<label><input type="checkbox" name="include_cached_tables" />Include Cache Tables in dump</label><br />-->
+					<input type="submit" name="send" value="Create Dump"/>
+				</fieldset>
+			</form>
+ENDFORM;
+	}
+}else{
+	echo "Localconf.php not Readable / not Found";
+}
+?>
+</body>
+</html>
